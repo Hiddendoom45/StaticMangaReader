a2825d24a0572f5ac4c7efc4aae382bffc5814bb
hiddendoom45 <hiddendoom45@gmail.com>
2019-03-20 17:53:55 -0400

    Linting and moving stuff around in reader

--- reader.js
+++ reader.js
@@ -1,8 +1,8 @@
 var currentpage = 0;
-var preload
+var preload;
 function loadJSON() {
     if(typeof chjson === 'undefined'){
-        setup()
+        setup();
     }
     else{
         var xobj = new XMLHttpRequest();
@@ -11,34 +11,34 @@ function loadJSON() {
         xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             data = JSON.parse(xobj.responseText);
-            setup()
+            setup();
         }
         };
         xobj.send(null);
+    }
+}
+function setup(){
+    chapters = data.pages;
+    document.getElementById('total').innerHTML = " "+data.pages.length
+    var pagenum = getParameterByName("page");
+    if(pagenum==="end"){
+        setpage(chapters.length);
+    }
+    else if(Number.isInteger(parseInt(pagenum))){
+        setpage(parseInt(pagenum)-1);
     }
     document.getElementById('mainimage').addEventListener('load',function(){
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     })
 }
-function setup(){
-    chapters = data.pages;
-    document.getElementById('total').innerHTML = " "+data.pages.length
-    var pagenum = getParameterByName("page")
-    if(pagenum==="end"){
-    setpage(chapters.length)
-    }
-    else if(Number.isInteger(parseInt(pagenum))){
-    setpage(parseInt(pagenum)-1)
-    }
-}
 function nextPage() {
     if(typeof chapters === 'undefined') return;
     if(currentpage>=chapters.length-1){
-       if(data.nextchapter!=='') window.location = data.nextchapter
+       if(data.nextchapter!=='') window.location = data.nextchapter;
     }
     else{
         setpage(++currentpage);
-        preload = ((new Image()).src = chapters[currentpage].page)
+        preload = ((new Image()).src = chapters[currentpage].page);
     }
 }
 function previousPage() {
@@ -47,17 +47,17 @@ function previousPage() {
         setpage(--currentpage);
     }
     else{
-        if(data.previouschapter!=='') window.location = data.previouschapter+"?page=end"
+        if(data.previouschapter!=='') window.location = data.previouschapter+"?page=end";
     }
 }
 function setpage(page) {
     if(typeof chapters === 'undefined') return;
     if(page<0) page = 0;
-    if(page>=chapters.length) page = chapters.length-1
+    if(page>=chapters.length) page = chapters.length-1;
     currentpage = page;
-    document.getElementById('mainimage').src=chapters[page].page
+    document.getElementById('mainimage').src=chapters[page].page;
     
-    document.getElementById('pageField').value=currentpage+1
+    document.getElementById('pageField').value=currentpage+1;
 }
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
